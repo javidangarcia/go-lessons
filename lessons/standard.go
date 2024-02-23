a7c2c6d42424c7af70ac4c5270dd70b5ba72fa9b
Javier Garcia <javierdangarcia@gmail.com>
2024-02-23 03:39:26 -0500

    added standard library lesson

---
+++ lessons/standard.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func standard() {
+
+	greeting := "hello there friends!"
+
+	fmt.Println(strings.Contains(greeting, "hello!"))
+	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	fmt.Println(strings.ToUpper(greeting))
+	fmt.Println(strings.Index(greeting, "fr"))
+	fmt.Println(strings.Split(greeting, " "))
+
+	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
+	sort.Ints(ages)
+	fmt.Println(ages)
+
+	index := sort.SearchInts(ages, 30)
+	fmt.Println(index)
+
+	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+	sort.Strings(names)
+	fmt.Println(names)
+
+	fmt.Println(sort.SearchStrings(names, "bowser"))
+
+}
