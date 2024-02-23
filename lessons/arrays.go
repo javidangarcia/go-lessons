be8d1bac897c1a29c310bd3adffcfa4277c692e4
Javier Garcia <javierdangarcia@gmail.com>
2024-02-23 03:41:18 -0500

    added arrays and slices lesson

---
+++ lessons/arrays.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func arrays() {
+
+	// arrays
+	var ages = [3]int{20, 25, 30} // OR var ages [3]int = [3]int{20, 25, 30}
+
+	names := [4]string{"yoshi", "mario", "peach", "bowser"}
+	names[1] = "luigi"
+
+	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
+
+	// slices (uses arrays under the hood)
+	var scores = []int{100, 50, 60}
+	scores[2] = 25
+	scores = append(scores, 85)
+
+	fmt.Println(scores, len(scores))
+
+	// slice ranges
+	rangeOne := names[1:3]
+	rangeTwo := names[2:]
+	rangeThree := names[:3]
+
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+	rangeOne = append(rangeOne, "koopa")
+
+	fmt.Println(rangeOne)
+
+}
